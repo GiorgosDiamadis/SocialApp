64e866ef3365762158d88568a25dc6efa9094207
Giorgos <diamantisgiorgos2@gmail.com>
2021-04-09 22:51:08 +0300

    commit

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -30,14 +30,15 @@ export default function Profile(props) {
     data: { getFriends: friends } = {},
   } = useQuery(GET_FRIENDS, {
     update() {},
-    variables: { ID: profileId },
+    variables: { ID: user.id },
   });
 
   const [addFriend] = useMutation(ADD_FRIEND, {
     variables: { profileId },
-    refetchQueries: [{ query: GET_FRIENDS, variables: { ID: profileId } }],
+    refetchQueries: [{ query: GET_FRIENDS, variables: { ID: user.id } }],
   });
 
+  console.log(friends);
   const isFriend = () => {
     return (
       friends.friends.findIndex(
@@ -76,6 +77,22 @@ export default function Profile(props) {
                       circular
                       centered
                     />
+                    <a
+                      onClick={() => addFriend()}
+                      className="page-title add-remove-friend"
+                    >
+                      {user.id !== profileId ? (
+                        loadingFriends ? (
+                          <h4>Loading...</h4>
+                        ) : isFriend() ? (
+                          "Remove Friend"
+                        ) : (
+                          "Add Friend"
+                        )
+                      ) : (
+                        ""
+                      )}
+                    </a>
                     <Card.Content>
                       <List>
                         <List.Item icon="user" content={profileInfo.username} />
@@ -128,141 +145,13 @@ export default function Profile(props) {
                 {loadingFriends ? (
                   <h1>Loading...</h1>
                 ) : (
-                  <ProfileCard key={"profile"} friends={friends.friends} />
+                  <ProfileCard key={"profile"} friends={profileInfo.friends} />
                 )}
               </div>
             )}
           </Grid.Column>
         </Grid.Row>
       </Grid>
-      {/* <Card className="profile">
-        <Image
-          src="https://cdn.iconscout.com/icon/free/png-256/avatar-373-456325.png"
-          size="medium"
-          circular
-          centered
-        />
-        <div></div>
-        <Card.Content>
-          <div>
-            {loadingInfo ? (
-              <h4>Loading...</h4>
-            ) : (
-              <div>
-                <h2 className="page-title">{profileInfo.username}</h2>
-                <a
-                  onClick={() => addFriend()}
-                  className="page-title add-remove-friend"
-                >
-                  {user.id !== profileId ? (
-                    loadingFriends ? (
-                      <h4>Loading...</h4>
-                    ) : isFriend() ? (
-                      "Remove Friend"
-                    ) : (
-                      "Add Friend"
-                    )
-                  ) : (
-                    ""
-                  )}
-                </a>
-              </div>
-            )}
-          </div>
-        </Card.Content>
-        <Card.Content>
-          <Grid>
-            <Grid.Row>
-              <Grid.Column width={5}>
-                {" "}
-                <h3 className="page-title">Personal details</h3>
-                {loadingInfo ? (
-                  <h4>Loading...</h4>
-                ) : (
-                  <div>
-                    <Card.Group>
-                      <Card fluid className="postCard">
-                        <Card.Content>
-                          <List>
-                            <List.Item
-                              icon="user"
-                              content={profileInfo.username}
-                            />
-                            <List.Item
-                              icon="mail"
-                              content={profileInfo.email}
-                            />
-                            <List.Item
-                              icon="calendar check outline"
-                              content={
-                                "Joined in " +
-                                moment(profileInfo.createdAt)
-                                  .utc()
-                                  .format("DD/MM/YYYY")
-                              }
-                            />
-                            <List.Item
-                              icon="marker"
-                              content={"Born at " + profileInfo.born}
-                            />
-                            <List.Item
-                              icon="marker"
-                              content={"Lives in " + profileInfo.livesIn}
-                            />
-                            <List.Item
-                              icon="marker"
-                              content={"Is from " + profileInfo.isFrom}
-                            />
-                            <List.Item
-                              icon="graduation cap"
-                              content={
-                                "Graduated at " + profileInfo.graduatedAt
-                              }
-                            />
-                          </List>
-                        </Card.Content>
-                        {user.id === profileId ? (
-                          <Button
-                            primary
-                            onClick={() =>
-                              props.history.push({
-                                pathname: `/profile/${user.id}/editInfo`,
-                                state: { profileInfo, profileId },
-                              })
-                            }
-                          >
-                            Edit
-                          </Button>
-                        ) : (
-                          ""
-                        )}
-                      </Card>
-                    </Card.Group>
-                    <h3 className="page-title">Friends</h3>
-                    {loadingFriends ? (
-                      <h1>Loading...</h1>
-                    ) : (
-                      <ProfileCard key={"profile"} friends={friends.friends} />
-                    )}
-                  </div>
-                )}
-              </Grid.Column>
-              <Grid.Column width={5}>
-                <h3 className="page-title">Latest Posts</h3>
-                {loadingInfo || loading ? (
-                  <h1>Loading...</h1>
-                ) : (
-                  posts
-                    .filter(
-                      (post) => post.user.username === profileInfo.username
-                    )
-                    .map((post) => <PostCard key={post.id} post={post} />)
-                )}
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Card.Content>
-      </Card> */}
     </div>
   );
 }
