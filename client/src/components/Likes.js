93bbceae52ad2aecef9473e33b3de8cb995d4614
Giorgos <diamantisgiorgos2@gmail.com>
2021-04-15 16:00:16 +0300

    commit

--- client/src/components/Likes.js
+++ client/src/components/Likes.js
@@ -1,17 +1,37 @@
 import React from "react";
-import { Button, Modal } from "semantic-ui-react";
+import { Button, Modal, List, Image } from "semantic-ui-react";
+import moment from "moment";
+
 export default function Likes({ likes, dispatch, open, size, props }) {
   return (
     <Modal size={size} open={open} onClose={() => dispatch({ type: "close" })}>
       <Modal.Header>Likes</Modal.Header>
       <Modal.Content>
         {likes.map((like) => (
-          <a
-            key={like.id}
-            onClick={() => props.history.push(`/profile/${like.user.id}`)}
-          >
-            {like.user.username}
-          </a>
+          <List divided relaxed>
+            <List.Item>
+              <Image
+                src="https://cdn.iconscout.com/icon/free/png-256/avatar-373-456325.png"
+                size="mini"
+                circular
+                centered
+              />
+              <List.Content>
+                <List.Header
+                  as="a"
+                  onClick={() => {
+                    dispatch({ type: "close" });
+                    props.history.push(`/profile/${like.user.id}`);
+                  }}
+                >
+                  {like.user.username}
+                </List.Header>
+                <List.Description as="a">
+                  {moment(like.createdAt.replace("T", " ")).fromNow()}
+                </List.Description>
+              </List.Content>
+            </List.Item>
+          </List>
         ))}
       </Modal.Content>
       <Modal.Actions>
