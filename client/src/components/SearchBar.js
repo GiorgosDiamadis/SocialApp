dc1321b38fb450837a3036d983243b8146125be3
Giorgos <diamantisgiorgos2@gmail.com>
2021-04-17 14:51:57 +0300

    commit

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Input } from "semantic-ui-react";
 
 export default function SearchBar() {
+  const [state, setState] = useState({
+    name: "",
+    typingTimeout: 0,
+  });
+  const searchBar = useRef(null);
+  const onChange = (event) => {
+    if (state.typingTimeout) {
+      clearTimeout(state.typingTimeout);
+    }
+    const searchBar = React.findDOMNode("searchUser");
+    console.log(searchBar);
+
+    searchBar.classList.add("loading");
+    setState({
+      name: event.target.value,
+      typingTimeout: setTimeout(function () {
+        searchBar.classList.remove("loading");
+      }, 1000),
+    });
+  };
+
   return (
-    <Input className="icon" size="mini" icon="search" placeholder="Search..." />
+    <Input
+      className={"icon"}
+      ref="searchUser"
+      size="mini"
+      icon="search"
+      placeholder="Search..."
+      onChange={onChange}
+    />
   );
 }
